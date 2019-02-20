601b4e368b4de5c266cf5ffddb35265c397c1c68
Maria Karyda <mariakarida@gmail.com>
2019-02-20 09:58:38 +0200

    Add an Order page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -91,33 +91,13 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        // this.setState({loading: true});
         // // alert('You continue!');
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice.toFixed('2'),
-        //     customer: {
-        //         name: 'Maria Karyda',
-        //         address: {
-        //             street: 'Teststreet 1',
-        //             zipCode: '41351',
-        //             country: 'Greece'
-        //         },
-        //         email: 'mariakarida@gmail.com'
-        //     },
-        //     deliveryMethod: 'fastest'
-        // }
-        // axios.post('/orders.json', order)
-        // .then ( response => {
-        //     this.setState({loading: false, purchasing: false});
-        // })
-        // .catch ( error => {
-        //     this.setState({loading: false, purchasing: false});
-        // });
+
         const queryParams = [];
         for (let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
+        queryParams.push('price=' + this.state.totalPrice.toFixed(2));
         const queryString = queryParams.join('&');
         this.props.history.push({
             pathname: 'checkout',
