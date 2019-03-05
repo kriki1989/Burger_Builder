355c5c600b4aa7f167aadd597d17201cd15a5afb
Maria Karyda <maria.ka@scopemarkets.com>
2019-03-05 15:50:00 +0200

    Change on index

--- src/index.js
+++ src/index.js
@@ -5,14 +5,18 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 import './index.css';
 import App from './App';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
+import { watchAuth } from './store/sagas/index';
 
-const composeEnhancers = process.env.NODE_ENV === 'developement' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+// const composeEnhancers = process.env.NODE_ENV === 'developement' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__  || compose;
+
 
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,
@@ -20,9 +24,13 @@ const rootReducer = combineReducers({
     auth: authReducer
 });
 
+const sagaMiddleware = createSagaMiddleware();
+
 const store = createStore(rootReducer, composeEnhancers(
-    applyMiddleware(thunk)
+    applyMiddleware(thunk, sagaMiddleware)
 ));
+
+sagaMiddleware.run(watchAuth);
 
 const app = (
     <Provider store={store}>
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -14,5 +14,6 @@ export {
     auth,
     logout,
     setAuthRedirectpath,
-    authCheckState
+    authCheckState,
+    logoutSucceed
 } from './auth';
